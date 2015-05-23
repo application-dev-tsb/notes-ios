d6751df672686f1b839291e0c0638d65e839612e
Master of Null <hack.of.all.codes@gmail.com>
2015-05-23 15:34:37 +0800

    working search controller
    cool job bro!!!

--- SwiftProjects/TableAndSearch/TableAndSearch/TableViewController.swift
+++ SwiftProjects/TableAndSearch/TableAndSearch/TableViewController.swift
@@ -8,90 +8,74 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    private var myData = [String]()
+    
+    private let buyType = ["Higher", "Lower"]
+    
+    //lesson learned: if its local, its gone
+    private let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        searchController.searchBar.scopeButtonTitles = buyType
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+        
+        tableView.tableHeaderView = searchController.searchBar
+        
+        //bug: search controller goes off-screen without this
+        definesPresentationContext = true
+        
+        //bug: size of search bar is zero if there is no scope buttons
+        searchController.searchBar.sizeToFit()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+    
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let number: Int = searchController.searchBar.text.toInt() {
+            let lower: Bool = searchController.searchBar.selectedScopeButtonIndex == 1
+
+            let range = lower ? number-10...number : number...number+10
+            
+            var newData = [String]()
+            for n in range  {
+                newData.append("Number \(n)")
+            }
+            
+            myData = newData
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - UISearchBarDelegate
+    
+    func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        updateSearchResultsForSearchController(searchController)
     }
 
-    // MARK: - Table view data source
+    // MARK: - TableViewController
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 100
+        return myData.count
     }
-
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
-
-        cell.textLabel?.text = "My Number \(indexPath.row)"
-
+        cell.textLabel?.text = myData[indexPath.row]
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
