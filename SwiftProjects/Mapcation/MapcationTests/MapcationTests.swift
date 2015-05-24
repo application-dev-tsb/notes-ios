5a43920c8b1c23513061ca1f5976c6030129a1ce
Master of Null <hack.of.all.codes@gmail.com>
2015-05-24 12:47:59 +0800

    added initial Mapcation project

---
+++ SwiftProjects/Mapcation/MapcationTests/MapcationTests.swift
@@ -0,0 +1,36 @@
+//
+//  MapcationTests.swift
+//  MapcationTests
+//
+//  Created by Lyndon Michael Bibera on 5/24/15.
+//  Copyright (c) 2015 Hack of All Codes. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MapcationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
